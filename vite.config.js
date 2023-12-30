ae8e89f2310f16b8d0a094b9cea6f745d8717a89
Maurice Wipf <maurice@gmx.de>
2023-12-30 16:30:29 +0100

    chore: Distinguish between vite dev and vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
+/** @type {import('vite').UserConfig} */
 import {defineConfig} from "vite";
 
-export default defineConfig({
-    build: {
-        rollupOptions: {
-            output: {
-                entryFileNames: `assets/[name].js`,
-                chunkFileNames: `assets/[name].js`,
-                assetFileNames: `assets/[name].[ext]`
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+    const temp = process.env;
+    console.log('process.env ', process.env );
+    if (command === 'serve') {
+        return {
+            // dev specific config
+        }
+    } else {
+        // command === 'build'
+        return {
+            // build specific config
+            root: 'src',
+            build: {
+                outDir: './dist2',
+                emptyOutDir: true,
+                rollupOptions: {
+                    output: {
+                        entryFileNames: `assets/[name].js`,
+                        chunkFileNames: `assets/[name].js`,
+                        assetFileNames: `assets/[name].[ext]`
+                    }
+                }
             }
         }
     }
