0a5539aba7408048636fc8c61472a37b05f0cb2a
Maurice Wipf <maurice@gmx.de>
2024-01-04 08:48:12 +0100

    fix: Style input (class .form-input is not applied for some reason)

--- src/multi-page-form/multi-page-form.ts
+++ src/multi-page-form/multi-page-form.ts
@@ -95,7 +95,7 @@ export class MultiPageForm extends TailwindElement(style) {
             <label htmlFor="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
             <div class="mt-2">
               <input id="email" name="email" type="email" autocomplete="email" required
-                     class="form-input"/>
+                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
           </div>
 
@@ -103,7 +103,7 @@ export class MultiPageForm extends TailwindElement(style) {
             <label htmlFor="fullName" class="block text-sm font-medium leading-6 text-gray-900">Full name</label>
             <div class="mt-2">
               <input id="fullName" name="fullName" type="text" autocomplete="current-fullName" required
-                     class="form-input"/>
+                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
           </div>
         </form>`;
@@ -130,7 +130,7 @@ export class MultiPageForm extends TailwindElement(style) {
           <div class="col-span-full">
             <label htmlFor="country" class="block text-sm font-medium leading-6 text-gray-900">Country</label>
             <div class="mt-2">
-              <select id="country" name="country" autocomplete="country-name" class="form-input">
+              <select id="country" name="country" autocomplete="country-name" class="form-input-2">
                 <option>United States</option>
                 <option>Canada</option>
                 <option>Mexico</option>
