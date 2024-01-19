eebef63c5ff4198b63afe8bb4ced8101304b0245
Maurice Wipf <maurice@gmx.de>
2024-01-19 08:18:45 +0100

    fix: Set input checked correctly

--- src/carsharing-availability/weekdays-checkboxes/weekdays-checkboxes.ts
+++ src/carsharing-availability/weekdays-checkboxes/weekdays-checkboxes.ts
@@ -13,31 +13,31 @@ export class WeekdaysCheckboxes extends TailwindElement() {
         <div class='bg-white rounded-md'>
           <div class='flex space-x-4'>
             <label class='flex items-center space-x-2'>
-              <input type='checkbox' class='form-checkbox text-blue-500' checked=${this.weekdays?.includes('SUN')}/>
+              <input type='checkbox' class='form-checkbox text-blue-500 rounded' .checked=${this.weekdays?.includes('SUN')}/>
               <span class='text-gray-700'>Sunday</span>
             </label>
             <label class='flex items-center space-x-2'>
-              <input type='checkbox' class='form-checkbox text-blue-500' checked=${this.weekdays?.includes('MON')}/>
+              <input type='checkbox' class='form-checkbox text-blue-500 rounded' .checked=${this.weekdays?.includes('MON')}/>
               <span class='text-gray-700'>Monday</span>
             </label>
             <label class='flex items-center space-x-2'>
-              <input type='checkbox' class='form-checkbox text-blue-500' checked=${this.weekdays?.includes('TUE')}/>
+              <input type='checkbox' class='form-checkbox text-blue-500 rounded' .checked=${this.weekdays?.includes('TUE')}/>
               <span class='text-gray-700'>Tuesday</span>
             </label>
             <label class='flex items-center space-x-2'>
-              <input type='checkbox' class='form-checkbox text-blue-500' checked=${this.weekdays?.includes('WED')}/>
+              <input type='checkbox' class='form-checkbox text-blue-500 rounded' .checked=${this.weekdays?.includes('WED')}/>
               <span class='text-gray-700'>Wednesday</span>
             </label>
             <label class='flex items-center space-x-2'>
-              <input type='checkbox' class='form-checkbox text-blue-500' checked=${this.weekdays?.includes('THU')}/>
+              <input type='checkbox' class='form-checkbox text-blue-500 rounded' .checked=${this.weekdays?.includes('THU')}/>
               <span class='text-gray-700'>Thursday</span>
             </label>
             <label class='flex items-center space-x-2'>
-              <input type='checkbox' class='form-checkbox text-blue-500' checked=${this.weekdays?.includes('FRI')}/>
+              <input type='checkbox' class='form-checkbox text-blue-500 rounded' .checked=${this.weekdays?.includes('FRI')}/>
               <span class='text-gray-700'>Friday</span>
             </label>
             <label class='flex items-center space-x-2'>
-              <input type='checkbox' class='form-checkbox text-blue-500' checked=${this.weekdays?.includes('SAT')}/>
+              <input type='checkbox' class='form-checkbox text-blue-500 rounded' .checked=${this.weekdays?.includes('SAT')}/>
               <span class='text-gray-700'>Saturday</span>
             </label>
           </div>
