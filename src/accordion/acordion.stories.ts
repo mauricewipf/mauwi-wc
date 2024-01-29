69f2395fab71d396fbb4c5cf61f9aa8fc197b82d
Maurice Wipf <maurice@gmx.de>
2024-01-29 19:30:42 +0100

    refactor(Accordion): Remove padding from story

--- src/accordion/acordion.stories.ts
+++ src/accordion/acordion.stories.ts
@@ -8,7 +8,7 @@ export default {
   tags: ['autodocs'],
   render: (args) => html`
     <div class="bg-white">
-      <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+      <div class="mx-auto max-w-7xl">
         <div class="mx-auto max-w-4xl divide-y divide-gray-900/10">
           <h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
           <mauwi-accordion
