08652778421a94d6d2ab74f3fae117b5b6840fd7
Maurice Wipf <maurice@gmx.de>
2024-02-02 23:58:59 +0100

    feat(MultiPageForm): Remove required and start at page 1

--- src/multi-page-form/multi-page-form.ts
+++ src/multi-page-form/multi-page-form.ts
@@ -11,7 +11,7 @@ export interface MultiPageFormProps {
 export class MultiPageForm extends TailwindElement(style) {
   @property() action: string;
   @state() numberPages: number = 4;
-  @state() currentPage: number = 4;
+  @state() currentPage: number = 1;
   data: any;
 
   private nextPage = () => {
@@ -121,7 +121,7 @@ export class MultiPageForm extends TailwindElement(style) {
             <div>
               <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
               <div class="mt-2">
-                <input id="email" name="email" type="email" autocomplete="email" required
+                <input id="email" name="email" type="email" autocomplete="email"
                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
               </div>
             </div>
@@ -129,7 +129,7 @@ export class MultiPageForm extends TailwindElement(style) {
             <div>
               <label for="fullName" class="block text-sm font-medium leading-6 text-gray-900">Full name</label>
               <div class="mt-2">
-                <input id="fullName" name="fullName" type="text" autocomplete="current-fullName" required
+                <input id="fullName" name="fullName" type="text" autocomplete="current-fullName"
                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
               </div>
             </div>
