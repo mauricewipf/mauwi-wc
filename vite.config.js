9a98998f03fcfd88957e20cdbb68e4e1ff338c38
Maurice Wipf <maurice@gmx.de>
2024-02-13 08:53:33 +0100

    feat: Add components to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,14 +16,22 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
                 rollupOptions: {
                     input: {
                         'index': 'src/index.ts',
+                        'accordion': 'src/accordion/index.ts',
+                        'bar-chart': 'src/bar-chart/index.ts',
+                        'calendar': 'src/calendar/index.ts',
+                        'carousel': 'src/carousel/index.ts',
+                        'carsharing-availability': 'src/carsharing-availability/index.ts',
+                        'combobox': 'src/combobox/index.ts',
+                        'data-table': 'src/data-table/index.ts',
+                        'datepicker': 'src/datepicker/index.ts',
+                        'dialog': 'src/dialog/index.ts',
                         'map-overlay': 'src/map-overlay/index.ts',
                         'mega-flyout': 'src/mega-flyout/index.ts',
                         'multi-page-form': 'src/multi-page-form/index.ts',
-                        'quote-cards': 'src/quote-cards/index.ts',
-                        'carsharing-availability': 'src/carsharing-availability/index.ts',
-                        'bar-chart': 'src/bar-chart/index.ts',
                         'personal-information-form': 'src/personal-information-form/index.ts',
-                        // Add
+                        'quote-cards': 'src/quote-cards/index.ts',
+                        'tabs': 'src/tabs/index.ts',
+                        // Add -> 'template': 'src/template/index.ts'
                     },
                     output: {
                         entryFileNames: `entry-[name].js`,
